38afe87cab8d624919bd0c0eaa7c150c0646b663
Benjamin Smith <bsmith@cabbage.org.uk>
2018-05-21 16:48:43 +0100

    Initial commit

---
+++ suspend-xscreensaver.pl
@@ -0,0 +1,1 @@
+$|++; my $ST = time; while ((my $m = $ARGV[0] - (time - $ST) / 60) > 0) { my $rv = system("xscreensaver-command -deactivate 2>&1 >/dev/null"); printf "%.1f mins remaining (%s)\r", $m, $rv; sleep($m > 2 ? 113 : $m * 60) } system("tput el")
