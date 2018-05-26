1b09fcbc96bb2d4e1cda0a782968d5319219e518
Benjamin Smith <bsmith@cabbage.org.uk>
2018-05-26 15:18:30 +0100

    Notes of screensaver control commands

---
+++ lock_and_blank.pl
@@ -0,0 +1,4 @@
+die "unimpl";
+
+__END__
+xscreensaver-command -lock && sleep 1 && xset dpms force off
--- suspend-xscreensaver.pl
+++ suspend-xscreensaver.pl
@@ -1,1 +1,5 @@
+die "unimpl";
+
+__END__
 $|++; my $ST = time; while ((my $m = $ARGV[0] - (time - $ST) / 60) > 0) { my $rv = system("xscreensaver-command -deactivate 2>&1 >/dev/null"); printf "%.1f mins remaining (%s)\r", $m, $rv; sleep($m > 2 ? 113 : $m * 60) } system("tput el")
+perl -E'$|++; my $ST = time; while ((my $m = $ARGV[0] - (time - $ST) / 60) > 0) { system("xscreensaver-command -deactivate >/dev/null"); printf "%.1f mins remaining\r", $m; sleep($m > 2 ? 113 : $m * 60) } system("tput el")' 85
